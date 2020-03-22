1e8dadee4ef14cbab5571aa382332c0b0e058cae
qiaosiyi <qsy2011815@163.com>
2020-03-22 22:36:30 +0800

    Update key.c

--- key.c
+++ key.c
@@ -11,39 +11,36 @@
 void KEY_Init(void){
   GPIO_InitTypeDef GPIO_InitStructure;
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB| RCC_APB2Periph_GPIOC,ENABLE);
-
   GPIO_InitStructure.GPIO_Pin=GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_11 | GPIO_Pin_12 ;
   GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPD;
   GPIO_Init(GPIOA,&GPIO_InitStructure);
-
-
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
   GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable,ENABLE);
   
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_4|
-                  GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8|
-                  GPIO_Pin_9;               //
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         //
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
-    GPIO_Init(GPIOB,&GPIO_InitStructure);
+                                GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8|
+                                GPIO_Pin_9;                //
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         //
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_10MHz;
+  GPIO_Init(GPIOB,&GPIO_InitStructure);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15;               //14AT 15CLEAR
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         //
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(GPIOC,&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15; //14AT 15CLEAR
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;         //
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOC,&GPIO_InitStructure);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;               //13LINK STATUS
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;         //
-    GPIO_Init(GPIOC,&GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;               //13LINK STATUS
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;            //
+  GPIO_Init(GPIOC,&GPIO_InitStructure);
 }
 
 
 u8* check_cmd(u8 *str)
 {
   char *strx=0;
-  if(USART_RX_STA&0X8000)    //æŽ¥æ”¶åˆ°ä¸€æ¬¡æ•°æ®äº†
+  if(USART_RX_STA&0X8000)    
   { 
-    USART_RX_BUF[USART_RX_STA&0X7FFF]=0;//æ·»åŠ ç»“æŸç¬?
+    USART_RX_BUF[USART_RX_STA&0X7FFF]=0;
     strx=strstr((const char*)USART_RX_BUF,(const char*)str);
   } 
   return (u8*)strx;
@@ -55,17 +52,17 @@ u8 send_cmd(u8 *cmd,u8 *ack,u16 waittime)
   USART_RX_STA=0;
   if((u32)cmd<=0XFF)
   {
-    while((USART1->SR&0X40)==0);//ç­‰å¾…ä¸Šä¸€æ¬¡æ•°æ®å‘é€å®Œæˆ?  
+    while((USART1->SR&0X40)==0);
     USART1->DR=(u32)cmd;
-  }else printf("%s\r\n",cmd);//å‘é€å‘½ä»?
-  if(ack&&waittime)    //éœ€è¦ç­‰å¾…åº”ç­?
+  }else printf("%s\r\n",cmd);
+  if(ack&&waittime)    
   {
-    while(--waittime)  //ç­‰å¾…å€’è®¡æ—?
+    while(--waittime)  
     {
       delay_ms(10);
-      if(USART_RX_STA&0X8000)//æŽ¥æ”¶åˆ°æœŸå¾…çš„åº”ç­”ç»“æžœ
+      if(USART_RX_STA&0X8000)
       {
-        if(check_cmd(ack))break;//å¾—åˆ°æœ‰æ•ˆæ•°æ® 
+        if(check_cmd(ack))break;
         USART_RX_STA=0;
       } 
     }
@@ -161,12 +158,12 @@ u8 *Read_KeyValue(void)
   setrow9_1();
 
   while(temp_byte[0] != byte[0] | temp_byte[1] != byte[1] | temp_byte[2] != byte[2] | temp_byte[3] != byte[3] | 
-    temp_byte[4] != byte[4] | temp_byte[5] != byte[5] | temp_byte[6] != byte[6] | temp_byte[7] != byte[7] | 
-    temp_byte[8] != byte[8])
+        temp_byte[4] != byte[4] | temp_byte[5] != byte[5] | temp_byte[6] != byte[6] | temp_byte[7] != byte[7] | 
+        temp_byte[8] != byte[8])
   {
     for(i = 0; i < 9; i++)
       temp_byte[i] = byte[i];
-    
+
     setrow0_1();delay_us(50);byte[0] |= read_inputdata();//get input col value  
     setrow1_1();delay_us(50);byte[1] |= read_inputdata();//get input col value  
     setrow2_1();delay_us(50);byte[2] |= read_inputdata();//get input col value  
@@ -178,7 +175,6 @@ u8 *Read_KeyValue(void)
     setrow8_1();delay_us(50);byte[8] |= read_inputdata();//get input col value    
     setrow9_1();
   }
-  
   return byte;
 }
 
@@ -194,50 +190,50 @@ u8 *map_2_key(u8 *read_byte)
   i = 1;
   if(read_byte[0])
   {
-    if((read_byte[0] & 0x80) == 0x80){byte[i] = key_esc;i++;if(i == 7){return byte;}}
-    if((read_byte[0] & 0x40) == 0x40){byte[i] = key_1;i++;  if(i == 7){return byte;}}
-    if((read_byte[0] & 0x20) == 0x20){byte[i] = key_2;i++;  if(i == 7){return byte;}}
-    if((read_byte[0] & 0x10) == 0x10){byte[i] = key_3;i++;  if(i == 7){return byte;}}
-    if((read_byte[0] & 0x08) == 0x08){byte[i] = key_4;i++;  if(i == 7){return byte;}}
-    if((read_byte[0] & 0x04) == 0x04){byte[i] = key_5;i++;  if(i == 7){return byte;}}
-    if((read_byte[0] & 0x02) == 0x02){byte[i] = key_6;i++;  if(i == 7){return byte;}}
-    if((read_byte[0] & 0x01) == 0x01){byte[i] = key_7;i++;  if(i == 7){return byte;}}
+    if((read_byte[0] & 0x80) == 0x80){byte[i] = key_esc;i++;      if(i == 7){return byte;}}
+    if((read_byte[0] & 0x40) == 0x40){byte[i] = key_1;i++;        if(i == 7){return byte;}}
+    if((read_byte[0] & 0x20) == 0x20){byte[i] = key_2;i++;        if(i == 7){return byte;}}
+    if((read_byte[0] & 0x10) == 0x10){byte[i] = key_3;i++;        if(i == 7){return byte;}}
+    if((read_byte[0] & 0x08) == 0x08){byte[i] = key_4;i++;        if(i == 7){return byte;}}
+    if((read_byte[0] & 0x04) == 0x04){byte[i] = key_5;i++;        if(i == 7){return byte;}}
+    if((read_byte[0] & 0x02) == 0x02){byte[i] = key_6;i++;        if(i == 7){return byte;}}
+    if((read_byte[0] & 0x01) == 0x01){byte[i] = key_7;i++;        if(i == 7){return byte;}}
   }
 
   if(read_byte[1])
   {
-    if((read_byte[1] & 0x80) == 0x80){byte[i] = key_tab;i++;if(i == 7){return byte;}}
-    if((read_byte[1] & 0x40) == 0x40){byte[i] = key_q;i++;  if(i == 7){return byte;}}
-    if((read_byte[1] & 0x20) == 0x20){byte[i] = key_w;i++;  if(i == 7){return byte;}}
-    if((read_byte[1] & 0x10) == 0x10){byte[i] = key_e;i++;  if(i == 7){return byte;}}
-    if((read_byte[1] & 0x08) == 0x08){byte[i] = key_r;i++;  if(i == 7){return byte;}}
-    if((read_byte[1] & 0x04) == 0x04){byte[i] = key_T;i++;  if(i == 7){return byte;}}
-    if((read_byte[1] & 0x02) == 0x02){byte[i] = key_y;i++;  if(i == 7){return byte;}}
-    if((read_byte[1] & 0x01) == 0x01){byte[i] = key_u;i++;  if(i == 7){return byte;}}
+    if((read_byte[1] & 0x80) == 0x80){byte[i] = key_tab;i++;      if(i == 7){return byte;}}
+    if((read_byte[1] & 0x40) == 0x40){byte[i] = key_q;i++;        if(i == 7){return byte;}}
+    if((read_byte[1] & 0x20) == 0x20){byte[i] = key_w;i++;        if(i == 7){return byte;}}
+    if((read_byte[1] & 0x10) == 0x10){byte[i] = key_e;i++;        if(i == 7){return byte;}}
+    if((read_byte[1] & 0x08) == 0x08){byte[i] = key_r;i++;        if(i == 7){return byte;}}
+    if((read_byte[1] & 0x04) == 0x04){byte[i] = key_T;i++;        if(i == 7){return byte;}}
+    if((read_byte[1] & 0x02) == 0x02){byte[i] = key_y;i++;        if(i == 7){return byte;}}
+    if((read_byte[1] & 0x01) == 0x01){byte[i] = key_u;i++;        if(i == 7){return byte;}}
   }
 
   if(read_byte[2])
   {
     if((read_byte[2] & 0x80) == 0x80){byte[i] = key_caps_lock;i++;if(i == 7){return byte;}}
-    if((read_byte[2] & 0x40) == 0x40){byte[i] = key_a;i++;  if(i == 7){return byte;}}
-    if((read_byte[2] & 0x20) == 0x20){byte[i] = key_s;i++;  if(i == 7){return byte;}}
-    if((read_byte[2] & 0x10) == 0x10){byte[i] = key_d;i++;  if(i == 7){return byte;}}
-    if((read_byte[2] & 0x08) == 0x08){byte[i] = key_f;i++;  if(i == 7){return byte;}}
-    if((read_byte[2] & 0x04) == 0x04){byte[i] = key_g;i++;  if(i == 7){return byte;}}
-    if((read_byte[2] & 0x02) == 0x02){byte[i] = key_h;i++;  if(i == 7){return byte;}}
-    if((read_byte[2] & 0x01) == 0x01){byte[i] = key_j;i++;  if(i == 7){return byte;}}
+    if((read_byte[2] & 0x40) == 0x40){byte[i] = key_a;i++;        if(i == 7){return byte;}}
+    if((read_byte[2] & 0x20) == 0x20){byte[i] = key_s;i++;        if(i == 7){return byte;}}
+    if((read_byte[2] & 0x10) == 0x10){byte[i] = key_d;i++;        if(i == 7){return byte;}}
+    if((read_byte[2] & 0x08) == 0x08){byte[i] = key_f;i++;        if(i == 7){return byte;}}
+    if((read_byte[2] & 0x04) == 0x04){byte[i] = key_g;i++;        if(i == 7){return byte;}}
+    if((read_byte[2] & 0x02) == 0x02){byte[i] = key_h;i++;        if(i == 7){return byte;}}
+    if((read_byte[2] & 0x01) == 0x01){byte[i] = key_j;i++;        if(i == 7){return byte;}}
   }
 
   if(read_byte[3])
   {
     if((read_byte[3] & 0x80) == 0x80){byte[0] |= L_Shi;}
-    if((read_byte[3] & 0x40) == 0x40){byte[i] = key_insert;i++;  if(i == 7){return byte;}}
-    if((read_byte[3] & 0x20) == 0x20){byte[i] = key_z;i++;  if(i == 7){return byte;}}
-    if((read_byte[3] & 0x10) == 0x10){byte[i] = key_x;i++;  if(i == 7){return byte;}}
-    if((read_byte[3] & 0x08) == 0x08){byte[i] = key_c;i++;  if(i == 7){return byte;}}
-    if((read_byte[3] & 0x04) == 0x04){byte[i] = key_v;i++;  if(i == 7){return byte;}}
-    if((read_byte[3] & 0x02) == 0x02){byte[i] = key_b;i++;  if(i == 7){return byte;}}
-    if((read_byte[3] & 0x01) == 0x01){byte[i] = key_n;i++;  if(i == 7){return byte;}}
+    if((read_byte[3] & 0x40) == 0x40){byte[i] = key_insert;i++;   if(i == 7){return byte;}}
+    if((read_byte[3] & 0x20) == 0x20){byte[i] = key_z;i++;        if(i == 7){return byte;}}
+    if((read_byte[3] & 0x10) == 0x10){byte[i] = key_x;i++;        if(i == 7){return byte;}}
+    if((read_byte[3] & 0x08) == 0x08){byte[i] = key_c;i++;        if(i == 7){return byte;}}
+    if((read_byte[3] & 0x04) == 0x04){byte[i] = key_v;i++;        if(i == 7){return byte;}}
+    if((read_byte[3] & 0x02) == 0x02){byte[i] = key_b;i++;        if(i == 7){return byte;}}
+    if((read_byte[3] & 0x01) == 0x01){byte[i] = key_n;i++;        if(i == 7){return byte;}}
   }
 
   if(read_byte[4])
@@ -245,56 +241,56 @@ u8 *map_2_key(u8 *read_byte)
     if((read_byte[4] & 0x80) == 0x80){byte[0] |= R_Ctl;}
     if((read_byte[4] & 0x40) == 0x40){byte[0] |= L_Gui;}
     if((read_byte[4] & 0x20) == 0x20){byte[0] |= L_Alt;}
-    if((read_byte[4] & 0x10) == 0x10){byte[i] = L_APP;i++;  if(i == 7){return byte;}}
-    if((read_byte[4] & 0x08) == 0x08){byte[i] = L_FN;i++;  if(i == 7){return byte;}}
-    if((read_byte[4] & 0x04) == 0x04){byte[i] = key_spacebar;i++;  if(i == 7){return byte;}}
-    if((read_byte[4] & 0x02) == 0x02){byte[i] = key_home;i++;  if(i == 7){return byte;}}
-    if((read_byte[4] & 0x01) == 0x01){byte[i] = key_end;i++;  if(i == 7){return byte;}}
+    if((read_byte[4] & 0x10) == 0x10){byte[i] = L_APP;i++;        if(i == 7){return byte;}}
+    if((read_byte[4] & 0x08) == 0x08){byte[i] = L_FN;i++;         if(i == 7){return byte;}}
+    if((read_byte[4] & 0x04) == 0x04){byte[i] = key_spacebar;i++; if(i == 7){return byte;}}
+    if((read_byte[4] & 0x02) == 0x02){byte[i] = key_home;i++;     if(i == 7){return byte;}}
+    if((read_byte[4] & 0x01) == 0x01){byte[i] = key_end;i++;      if(i == 7){return byte;}}
   }
 
   if(read_byte[5])
   {
-    if((read_byte[5] & 0x80) == 0x80){byte[i] = key_8;i++;if(i == 7){return byte;}}
-    if((read_byte[5] & 0x40) == 0x40){byte[i] = key_9;i++;  if(i == 7){return byte;}}
-    if((read_byte[5] & 0x20) == 0x20){byte[i] = key_0;i++;  if(i == 7){return byte;}}
-    if((read_byte[5] & 0x10) == 0x10){byte[i] = key_minus;i++;  if(i == 7){return byte;}}
-    if((read_byte[5] & 0x08) == 0x08){byte[i] = key_equal;i++;  if(i == 7){return byte;}}
-    if((read_byte[5] & 0x04) == 0x04){byte[i] = key_grave;i++;  if(i == 7){return byte;}}
+    if((read_byte[5] & 0x80) == 0x80){byte[i] = key_8;i++;          if(i == 7){return byte;}}
+    if((read_byte[5] & 0x40) == 0x40){byte[i] = key_9;i++;          if(i == 7){return byte;}}
+    if((read_byte[5] & 0x20) == 0x20){byte[i] = key_0;i++;          if(i == 7){return byte;}}
+    if((read_byte[5] & 0x10) == 0x10){byte[i] = key_minus;i++;      if(i == 7){return byte;}}
+    if((read_byte[5] & 0x08) == 0x08){byte[i] = key_equal;i++;      if(i == 7){return byte;}}
+    if((read_byte[5] & 0x04) == 0x04){byte[i] = key_grave;i++;      if(i == 7){return byte;}}
     if((read_byte[5] & 0x02) == 0x02){byte[i] = key_backslash;i++;  if(i == 7){return byte;}}
     if((read_byte[5] & 0x01) == 0x01){byte[i] = key_backspace;i++;  if(i == 7){return byte;}}
   }
 
   if(read_byte[6])
   {
-    if((read_byte[6] & 0x80) == 0x80){byte[i] = key_i;i++;if(i == 7){return byte;}}
-    if((read_byte[6] & 0x40) == 0x40){byte[i] = key_k;i++;  if(i == 7){return byte;}}
-    if((read_byte[6] & 0x20) == 0x20){byte[i] = key_l;i++;  if(i == 7){return byte;}}
+    if((read_byte[6] & 0x80) == 0x80){byte[i] = key_i;i++;          if(i == 7){return byte;}}
+    if((read_byte[6] & 0x40) == 0x40){byte[i] = key_k;i++;          if(i == 7){return byte;}}
+    if((read_byte[6] & 0x20) == 0x20){byte[i] = key_l;i++;          if(i == 7){return byte;}}
     if((read_byte[6] & 0x10) == 0x10){byte[i] = key_semicolon;i++;  if(i == 7){return byte;}}
-    if((read_byte[6] & 0x08) == 0x08){byte[i] = key_o;i++;  if(i == 7){return byte;}}
-    if((read_byte[6] & 0x04) == 0x04){byte[i] = key_p;i++;  if(i == 7){return byte;}}
-    if((read_byte[6] & 0x02) == 0x02){byte[i] = key_brace1;i++;  if(i == 7){return byte;}}
-    if((read_byte[6] & 0x01) == 0x01){byte[i] = key_brace2;i++;  if(i == 7){return byte;}}
+    if((read_byte[6] & 0x08) == 0x08){byte[i] = key_o;i++;          if(i == 7){return byte;}}
+    if((read_byte[6] & 0x04) == 0x04){byte[i] = key_p;i++;          if(i == 7){return byte;}}
+    if((read_byte[6] & 0x02) == 0x02){byte[i] = key_brace1;i++;     if(i == 7){return byte;}}
+    if((read_byte[6] & 0x01) == 0x01){byte[i] = key_brace2;i++;     if(i == 7){return byte;}}
   }
 
   if(read_byte[7])
   {
-    if((read_byte[7] & 0x80) == 0x80){byte[i] = key_m;i++;if(i == 7){return byte;}}
-    if((read_byte[7] & 0x40) == 0x40){byte[i] = key_comma;i++;  if(i == 7){return byte;}}
+    if((read_byte[7] & 0x80) == 0x80){byte[i] = key_m;i++;          if(i == 7){return byte;}}
+    if((read_byte[7] & 0x40) == 0x40){byte[i] = key_comma;i++;      if(i == 7){return byte;}}
     if((read_byte[7] & 0x20) == 0x20){byte[i] = key_full_stop;i++;  if(i == 7){return byte;}}
-    if((read_byte[7] & 0x10) == 0x10){byte[i] = key_slash;i++;  if(i == 7){return byte;}}
+    if((read_byte[7] & 0x10) == 0x10){byte[i] = key_slash;i++;      if(i == 7){return byte;}}
     if((read_byte[7] & 0x08) == 0x08){byte[0] |= R_Shi;}
     if((read_byte[7] & 0x04) == 0x04){byte[i] = key_quotation_mark;i++;  if(i == 7){return byte;}}
-    if((read_byte[7] & 0x02) == 0x02){byte[i] = key_enter;i++;  if(i == 7){return byte;}}
-    if((read_byte[7] & 0x01) == 0x01){byte[i] = key_delete;i++;  if(i == 7){return byte;}}
+    if((read_byte[7] & 0x02) == 0x02){byte[i] = key_enter;i++;      if(i == 7){return byte;}}
+    if((read_byte[7] & 0x01) == 0x01){byte[i] = key_delete;i++;     if(i == 7){return byte;}}
   }
 
   if(read_byte[8])
   {
-    if((read_byte[8] & 0x10) == 0x10){byte[i] = key_app;i++;  if(i == 7){return byte;}}
-    if((read_byte[8] & 0x08) == 0x08){byte[i] = key_left;i++;  if(i == 7){return byte;}}
-    if((read_byte[8] & 0x04) == 0x04){byte[i] = key_down;i++;  if(i == 7){return byte;}}
-    if((read_byte[8] & 0x02) == 0x02){byte[i] = key_up;i++;  if(i == 7){return byte;}}
-    if((read_byte[8] & 0x01) == 0x01){byte[i] = key_right;i++;  if(i == 7){return byte;}}
+    if((read_byte[8] & 0x10) == 0x10){byte[i] = key_app;i++;        if(i == 7){return byte;}}
+    if((read_byte[8] & 0x08) == 0x08){byte[i] = key_left;i++;       if(i == 7){return byte;}}
+    if((read_byte[8] & 0x04) == 0x04){byte[i] = key_down;i++;       if(i == 7){return byte;}}
+    if((read_byte[8] & 0x02) == 0x02){byte[i] = key_up;i++;         if(i == 7){return byte;}}
+    if((read_byte[8] & 0x01) == 0x01){byte[i] = key_right;i++;      if(i == 7){return byte;}}
   }
 
   for(i = 1; i < 9; i++)
@@ -629,24 +625,24 @@ u8 fn_fnc(u8 key)
 {
   switch(key)
   {
-  case key_1: return key_F1;
-  case key_2: return key_F2;
-  case key_3: return key_F3;
-  case key_4: return key_F4;
-  case key_5: return key_F5;
-  case key_6: return key_F6;
-  case key_7: return key_F7;
-  case key_8: return key_F8;
-  case key_9: return key_F9;
-  case key_0: return key_F10;
+  case key_1:     return key_F1;
+  case key_2:     return key_F2;
+  case key_3:     return key_F3;
+  case key_4:     return key_F4;
+  case key_5:     return key_F5;
+  case key_6:     return key_F6;
+  case key_7:     return key_F7;
+  case key_8:     return key_F8;
+  case key_9:     return key_F9;
+  case key_0:     return key_F10;
   case key_minus: return key_F11;
   case key_equal: return key_F12;
-  case key_w: return vol_up;
-  case key_s: return mute;
-  case key_x: return vol_down;
-  case key_home: return key_page_up;
-  case key_end: return key_page_down;
-  default: return 0;
+  case key_w:     return vol_up;
+  case key_s:     return mute;
+  case key_x:     return vol_down;
+  case key_home:  return key_page_up;
+  case key_end:   return key_page_down;
+  default:        return 0;
   }
 }
 
@@ -654,20 +650,20 @@ u8 app_fnc(u8 key)
 {
   switch(key)
   {
-  case key_1: return APP_1;
-  case key_2: return APP_2;
-  case key_3: return APP_3;
-  case key_4: return APP_4;
-  case key_5: return APP_5;
-  case key_6: return APP_6;
-  case key_7: return APP_7;
-  case key_8: return APP_8;  // change mode
-  case key_9: return APP_9;  // clear
-  case key_0: return APP_0;  // pair
+  case key_1:     return APP_1;
+  case key_2:     return APP_2;
+  case key_3:     return APP_3;
+  case key_4:     return APP_4;
+  case key_5:     return APP_5;
+  case key_6:     return APP_6;
+  case key_7:     return APP_7;
+  case key_8:     return APP_8;  // change mode
+  case key_9:     return APP_9;  // clear
+  case key_0:     return APP_0;  // pair
 
-  case key_g: return APP_G;  // www.google.com
-  case key_b: return APP_B;  // www.baidu.com
-  case key_n: return APP_N;  // www.126.com
-  default: return 0;
+  case key_g:     return APP_G;  // www.google.com
+  case key_b:     return APP_B;  // www.baidu.com
+  case key_n:     return APP_N;  // www.126.com
+  default:        return 0;
   }
 }
